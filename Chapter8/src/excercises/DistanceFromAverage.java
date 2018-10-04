bd036d049ac763515c69379309d77be64c8cb550
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-10-04 08:58:18 -0500

    update

--- Chapter8/src/excercises/DistanceFromAverage.java
+++ Chapter8/src/excercises/DistanceFromAverage.java
@@ -7,22 +7,25 @@ public class DistanceFromAverage
 
 	public static void main(String[] args) 
 	{
-	double[] arrayNums = new double[20];
+		Scanner input = new Scanner(System.in);
+		double total = 0;
+		double average;
+		int quit = 0;
+		double[] arrayNums = new double[20];
+	while ( quit != 99999) {
+	for(int i = 0; i < arrayNums.length;i++) 
+	{
+		System.out.println("Enter a Number >> ");
+		arrayNums[i] = input.nextDouble();
+		
+		total = total + arrayNums[i];
 	
-	Scanner input = new Scanner(System.in);
-	int quit = 0;
-	int i = 0;
-	double total = 0;
-	while (quit != 99999) {
-	System.out.println("Enter a number >> ");
-	arrayNums[i] = input.nextInt();
-	
-	total = total + arrayNums[i];
-	System.out.println("To quit type 99999");
-	quit = input.nextInt();
-	
-	
-
+		
+	}
+		average = total / arrayNums.length;
+		System.out.println(average);
+		System.out.println("To quit type 99999");
+		quit = input.nextInt();
 	}
 	}
 
