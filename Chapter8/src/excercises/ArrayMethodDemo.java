7adaa44bc06eff309f3704df05284f882637e4b3
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-10-04 09:55:57 -0500

    update

--- Chapter8/src/excercises/ArrayMethodDemo.java
+++ Chapter8/src/excercises/ArrayMethodDemo.java
@@ -1,10 +1,42 @@
 package excercises;
 
-public class ArrayMethodDemo {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ArrayMethodDemo 
+{
 
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		double[] arrayNums = new double[10];
+		
+		for(int i = 0; i < arrayNums.length;i++) 
+		{
+			System.out.println("Enter a Number >> ");
+			arrayNums[i] = input.nextDouble();
+
+			displayIntegers(arrayNums[i]);
+			displayReverse(arrayNums[i]);
+			displaySum(arrayNums[i]);
+			displayAverage(arrayNums[i]); 
+		}
 	}
-
+	public static void displayIntegers(double arrayNums) 
+	{
+		for(int j = 0; j < arrayNums;j++) {
+			System.out.println(j);
+		}
+	}
+	public static void displayReverse(double arrayNums) 
+	{
+		
+	}
+	public static void displaySum(double arrayNums) 
+	{
+		
+	}
+	public static void displayAverage(double arrayNums) 
+	{
+		
+	}
 }
